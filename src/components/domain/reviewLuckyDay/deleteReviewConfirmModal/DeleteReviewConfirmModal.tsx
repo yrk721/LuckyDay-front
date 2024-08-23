aa140b80ff94ce6658e86133d9a7e3f83c314840
yrk721 <yoorae0721@naver.com>
2024-08-24 00:26:30 +0900

    chore: 불필요한 fragment 제거 (#192)

--- src/components/domain/reviewLuckyDay/deleteReviewConfirmModal/DeleteReviewConfirmModal.tsx
+++ src/components/domain/reviewLuckyDay/deleteReviewConfirmModal/DeleteReviewConfirmModal.tsx
@@ -15,36 +15,34 @@ export default function DeleteReviewConfirmModal({
   const theme = useTheme();
 
   return (
-    <>
-      <BaseModal>
-        <S.ModalContainer>
-          <S.Text_h1>정말로 삭제하시겠어요?</S.Text_h1>
-          <S.Logo_Sad />
-          <S.Text_h2>
-            럭키데이 활동 기록이 삭제됩니다. <br />
-            삭제된 정보는 복구할 수 없습니다. <br />
-            <br />
-          </S.Text_h2>
-          <S.ButtonBox>
-            <SvgButton
-              label="취소"
-              onClick={onClose}
-              icon={<ShortBoxIcon />}
-              width="100px"
-              height="42px"
-            ></SvgButton>
-            <SvgButton
-              label="삭제하기"
-              onClick={onDelete}
-              icon={<ShortBoxIcon />}
-              textColor={theme.colors.white}
-              fillColor={theme.colors.purple}
-              width="100px"
-              height="42px"
-            ></SvgButton>
-          </S.ButtonBox>
-        </S.ModalContainer>
-      </BaseModal>
-    </>
+    <BaseModal>
+      <S.ModalContainer>
+        <S.Text_h1>정말로 삭제하시겠어요?</S.Text_h1>
+        <S.Logo_Sad />
+        <S.Text_h2>
+          럭키데이 활동 기록이 삭제됩니다. <br />
+          삭제된 정보는 복구할 수 없습니다. <br />
+          <br />
+        </S.Text_h2>
+        <S.ButtonBox>
+          <SvgButton
+            label="취소"
+            onClick={onClose}
+            icon={<ShortBoxIcon />}
+            width="100px"
+            height="42px"
+          ></SvgButton>
+          <SvgButton
+            label="삭제하기"
+            onClick={onDelete}
+            icon={<ShortBoxIcon />}
+            textColor={theme.colors.white}
+            fillColor={theme.colors.purple}
+            width="100px"
+            height="42px"
+          ></SvgButton>
+        </S.ButtonBox>
+      </S.ModalContainer>
+    </BaseModal>
   );
 }
