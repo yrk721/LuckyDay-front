6edbd71a78c35698a2086e7e5b4c21978419921c
yrk721 <yoorae0721@naver.com>
2024-10-13 00:52:44 +0900

    feat: 튜토리얼 Steps 컴포넌트 구조 설계 및 Step0, Step24 컴포넌트 생성 (#201)

---
+++ src/components/tutorial/steps/index.ts
@@ -0,0 +1,11 @@
+import Step0_Welcome from "./step0/Step0_Welcome";
+import Step1_CreateCycle1 from "./step1/Step1_CreateCycle1";
+// NOTE: 이 곳에 나머지 step 추가
+import Step24_Finish from "./step24/Step24_Finish";
+
+export const TutorialSteps = {
+  Step0_Welcome,
+  Step1_CreateCycle1,
+  // NOTE: 다른 모든 Step 컴포넌트들 추가
+  Step24_Finish,
+};
---
+++ src/components/tutorial/steps/step24/Step24_Finish.tsx
@@ -0,0 +1,15 @@
+import { LogoTooltip, TutorialTextBox } from "components/tutorial";
+
+export default function Step24_Finish() {
+  return (
+    <>
+      <LogoTooltip />
+      <TutorialTextBox>
+        저와 함께해 주셔서 감사해요! <br /> <br />
+        이제 여러분만의 <br />
+        럭키 데이를 즐길 시간이에요. <br />
+        <br />이 텍스트 박스를 클릭하면 시작할 수 있어요.
+      </TutorialTextBox>
+    </>
+  );
+}
