eec0e5e361d624c195c4c09c1c0ed2dbed3165e2
yrk721 <yoorae0721@naver.com>
2024-10-15 03:35:37 +0900

    feat: tutorialTexts 생성 (#201)

--- src/components/tutorial/steps/step0/Step0_Welcome.tsx
+++ src/components/tutorial/steps/step0/Step0_Welcome.tsx
@@ -4,12 +4,7 @@ export default function Step0_Welcome() {
   return (
     <>
       <LogoTooltip />
-      <TutorialTextBox>
-        안녕하세요! <br />
-        럭키 데이에 오신 것을 환영해요. <br /> <br />
-        제가 럭키 데이를 만드는 방법을 보여드릴까요? <br />
-        아래 버튼을 한 번 더 클릭해 보세요.
-      </TutorialTextBox>
+      <TutorialTextBox currentStep={0} />
     </>
   );
 }
--- src/components/tutorial/steps/step24/Step24_Finish.tsx
+++ src/components/tutorial/steps/step24/Step24_Finish.tsx
@@ -4,12 +4,7 @@ export default function Step24_Finish() {
   return (
     <>
       <LogoTooltip />
-      <TutorialTextBox>
-        저와 함께해 주셔서 감사해요! <br /> <br />
-        이제 여러분만의 <br />
-        럭키 데이를 즐길 시간이에요. <br />
-        <br />이 텍스트 박스를 클릭하면 시작할 수 있어요.
-      </TutorialTextBox>
+      <TutorialTextBox currentStep={24} />
     </>
   );
 }
