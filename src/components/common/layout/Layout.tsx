986b6259ab4d19b2f6970bb62d8f78a2818d723c
yrk721 <yoorae0721@naver.com>
2024-10-21 00:52:44 +0900

    fix: merge conflict 해결, 누락 코드 반영

--- src/components/common/layout/Layout.tsx
+++ src/components/common/layout/Layout.tsx
@@ -1,25 +1,27 @@
 import * as S from "./Layout.styled";
-import * as S from "./Layout.styled";
 import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./header";
 import { TutorialLayout, useTutorial } from "components/tutorial";
-import { TutorialLayout, useTutorial } from "components/tutorial";
 
 export default function Layout() {
-  export default function Layout() {
-    const { pathname } = useLocation();
-    const { isTutorialActive } = useTutorial();
-    const { isTutorialActive } = useTutorial();
+  const { pathname } = useLocation();
+  const { isTutorialActive } = useTutorial();
 
-    return (
-      <S.LayoutContainer>
-        <S.Layout>
-          {!(pathname === "/loading" || pathname === "/404") && <Header />}
-          {!(pathname === "/loading" || pathname === "/404") && <Header />}
+  const isHeaderVisible = !(pathname === "/404" || pathname === "/loading");
+
+  return (
+    <S.LayoutContainer>
+      <S.Layout>
+        {isHeaderVisible && (
+          <S.HeaderContainer>
+            <Header />
+          </S.HeaderContainer>
+        )}
+        <S.Content>
           <Outlet />
-          {isTutorialActive && <TutorialLayout />}
-        </S.Layout>
-      </S.LayoutContainer>
-    );
-  }
+        </S.Content>
+        {isTutorialActive && <TutorialLayout />}
+      </S.Layout>
+    </S.LayoutContainer>
+  );
 }
