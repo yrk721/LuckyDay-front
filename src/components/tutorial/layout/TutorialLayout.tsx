9c4e458bcef4137fea05ba31910f5b51eb86c6cb
yoorae <yoorae0721@naver.com>
2024-11-02 00:43:00 +0900

    feat: renderHighlight, 조건부 렌더링 로직 수정 (#221)

--- src/components/tutorial/layout/TutorialLayout.tsx
+++ src/components/tutorial/layout/TutorialLayout.tsx
@@ -2,7 +2,6 @@ import * as S from "./TutorialLayout.styled";
 import React, { useState, useContext } from "react";
 import { TutorialContext } from "../context/TutorialContext";
 import { useTutorial } from "components/tutorial/hooks";
-import { TUTORIAL_STEP_ORDER } from "../steps/tutorialSteps";
 import { TutorialTextBox, CloseTutorialConfirmModal } from "components";
 import { CloseIcon, CircleBoxIcon } from "assets";
 import { SvgFrame } from "components/common";
@@ -18,54 +17,95 @@ export default function TutorialLayout() {
     setShowConfirmModal(true);
   };
 
-  const handleConfirm = () => {
-    endTutorial();
-  };
-
   if (!isTutorialActive) return null;
 
-  const isWelcomeOrFinishStep =
-    currentStep === 0 || currentStep === TUTORIAL_STEP_ORDER.length - 1;
+  const isWelcomeStep = currentStep === 0;
 
-  const renderHighlightedButton = () => {
-    console.log("Rendering highlighted button:", highlightedButton);
-    if (!highlightedButton) return null;
-
-    let style: React.CSSProperties = {
-      position: "absolute",
-      zIndex: 999,
-    };
-
-    if (highlightedButton.selector) {
-      const element = document.querySelector(highlightedButton.selector);
-      if (element) {
-        const rect = element.getBoundingClientRect();
-        style = {
-          ...style,
-          top: `${rect.top}px`,
-          left: `${rect.left}px`,
-          width: `${rect.width}px`,
-          height: `${rect.height}px`,
-        };
-      }
-    } else if (highlightedButton.position) {
-      style = { ...style, ...highlightedButton.position };
+  const renderHighlightedContent = () => {
+    if (!highlightedButton) {
+      return (
+        <S.TutorialTextBoxWrapper
+          style={isWelcomeStep ? undefined : tutorialTextBoxPosition}
+        >
+          <TutorialTextBox currentStep={currentStep} />
+        </S.TutorialTextBoxWrapper>
+      );
     }
 
+    const renderHighlight = () => {
+      if (!highlightedButton.highlightTarget) return null;
+
+      let style: React.CSSProperties = {
+        position: "absolute",
+      };
+
+      if (highlightedButton.highlightTarget.selector) {
+        const element = document.querySelector(
+          highlightedButton.highlightTarget.selector
+        );
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          style = {
+            ...style,
+            top: `${rect.top}px`,
+            left: `${rect.left}px`,
+            width: `${rect.width}px`,
+            height: `${rect.height}px`,
+          };
+        }
+      }
+
+      return (
+        <S.HighlightedButtonWrapper className="with-component" style={style}>
+          {highlightedButton.highlightTarget.component &&
+            React.cloneElement(
+              highlightedButton.highlightTarget.component as React.ReactElement,
+              {
+                style: { width: "100%", height: "100%" },
+              }
+            )}
+        </S.HighlightedButtonWrapper>
+      );
+    };
+
+    const renderTextBox = () => {
+      const style = highlightedButton.position || {};
+
+      return (
+        <S.HighlightedButtonWrapper className="with-text-box" style={style}>
+          <TutorialTextBox
+            currentStep={currentStep}
+            {...highlightedButton.textBoxProps}
+          >
+            {highlightedButton.textBoxes?.[0]}
+          </TutorialTextBox>
+        </S.HighlightedButtonWrapper>
+      );
+    };
+
     return (
-      <S.HighlightedButtonWrapper style={style}>
-        {React.cloneElement(highlightedButton.component as React.ReactElement, {
-          style: { width: "100%", height: "100%" },
-        })}
-      </S.HighlightedButtonWrapper>
+      <>
+        {renderHighlight()}
+        {renderTextBox()}
+      </>
     );
   };
 
   return (
     <>
-      {!isWelcomeOrFinishStep && (
+      {showConfirmModal && (
+        <CloseTutorialConfirmModal
+          onClose={() => setShowConfirmModal(false)}
+          onConfirm={endTutorial}
+        />
+      )}
+      {!isWelcomeStep && (
         <S.TutorialFullPage>
           <TutorialPage />
+          <S.LogoBox>
+            <S.Logo_Basic />
+            Lucky Day
+          </S.LogoBox>
         </S.TutorialFullPage>
       )}
       <S.TutorialOverlay>
@@ -78,22 +118,10 @@ export default function TutorialLayout() {
               </span>
             </S.CloseButton>
           </S.CloseButtonWrapper>
-          {isWelcomeOrFinishStep && <TutorialPage />}
+          {isWelcomeStep && <TutorialPage />}
         </S.TutorialContent>
-
-        <S.TutorialTextBoxWrapper
-          style={isWelcomeOrFinishStep ? undefined : tutorialTextBoxPosition}
-        >
-          <TutorialTextBox currentStep={currentStep} />
-        </S.TutorialTextBoxWrapper>
       </S.TutorialOverlay>
-      {!isWelcomeOrFinishStep && renderHighlightedButton()}
-      {showConfirmModal && (
-        <CloseTutorialConfirmModal
-          onClose={() => setShowConfirmModal(false)}
-          onConfirm={handleConfirm}
-        />
-      )}
+      {renderHighlightedContent()}
     </>
   );
 }
