27358e1180027a254dbeda47f361b50f9db9268c
yoorae <yoorae0721@naver.com>
2024-11-21 21:17:56 +0900

    style: android splash screen 수정 (#235)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import react from "@vitejs/plugin-react";
-import svgr from "@svgr/rollup";
 import viteTsconfigPaths from "vite-tsconfig-paths";
-// NOTE: src 경로 설정을 위해 추가
-// (https://velog.io/@otterji/Vite-typescript-%ED%99%98%EA%B2%BD%EC%97%90%EC%84%9C-path-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0 참고)
-import { defineConfig, ConfigEnv, UserConfig } from "vite";
+import svgr from "@svgr/rollup";
+import {
+  defineConfig,
+  ConfigEnv,
+  UserConfig,
+  Plugin as VitePlugin,
+} from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import removeConsole from "vite-plugin-remove-console";
 
-// https://vitejs.dev/config/
 export default defineConfig((env: ConfigEnv): UserConfig => {
   const plugins = [
     react(),
@@ -31,7 +33,7 @@ export default defineConfig((env: ConfigEnv): UserConfig => {
         short_name: "Lucky Day",
         description: "무작위로 찾아오는 나만의 특별한 날",
         theme_color: "#faf7f3",
-        background_color: "#faf7f3",
+        background_color: "#FFB43E",
         display: "standalone",
         start_url: "/",
         scope: "/",
@@ -116,7 +118,7 @@ export default defineConfig((env: ConfigEnv): UserConfig => {
         ],
       },
     }),
-  ].filter(Boolean) as Plugin[];
+  ].filter(Boolean) as VitePlugin[];
 
   return {
     plugins,
