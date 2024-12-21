fe02355f6fba816d8ca74aecbb2e853fc6ce8fbe
yoorae <yoorae0721@naver.com>
2024-12-21 16:53:21 +0900

    refactor: 공지사항 상수 파일 분리 및 리스트 페이지 로직 수정 (#237)

--- src/pages/noticeList/NoticeListPage.tsx
+++ src/pages/noticeList/NoticeListPage.tsx
@@ -1,6 +1,21 @@
 import * as S from "./NoticeListPage.styled";
-import { SingleButtonLayout } from "components";
 import { useNavigate } from "react-router-dom";
+import { SingleButtonLayout } from "components";
+import { NOTICE_DETAILS } from "constants/notice";
+
+interface NoticeMenuProps {
+  id: string;
+  title: string;
+  date: string;
+  onClick: (id: string) => void;
+}
+
+const NoticeMenu = ({ id, title, date, onClick }: NoticeMenuProps) => (
+  <S.MenuBox onClick={() => onClick(id)}>
+    {title}
+    <S.DateBox>{date}</S.DateBox>
+  </S.MenuBox>
+);
 
 export default function NoticeListPage() {
   const navigate = useNavigate();
@@ -13,24 +28,15 @@ export default function NoticeListPage() {
     <SingleButtonLayout>
       <S.TitleBox>공지사항</S.TitleBox>
       <S.ContentsBox>
-        <S.MenuBox onClick={() => handleMoveToNoticeDetail("4")}>
-          럭키데이 웹사이트를 스마트폰 홈 화면에서 만나보세요.
-          <S.DateBox>2024년 11월 30일</S.DateBox>
-        </S.MenuBox>
-        {/* NOTE: 튜토리얼 기능 추가 후 주석 해제 */}
-
-        {/* <S.MenuBox onClick={() => handleMoveToNoticeDetail("3")}>
-          튜토리얼 기능이 추가되었어요.
-          <S.DateBox>2024년 11월 12일</S.DateBox>
-        </S.MenuBox> */}
-        <S.MenuBox onClick={() => handleMoveToNoticeDetail("2")}>
-          럭키데이 서비스의 만족도를 평가해 주세요.
-          <S.DateBox>2024년 07월 01일</S.DateBox>
-        </S.MenuBox>
-        <S.MenuBox onClick={() => handleMoveToNoticeDetail("1")}>
-          일상에 특별함을 더해줄 럭키데이를 소개합니다.
-          <S.DateBox>2024년 06월 01일</S.DateBox>
-        </S.MenuBox>
+        {NOTICE_DETAILS.map((notice) => (
+          <NoticeMenu
+            key={notice.id}
+            id={notice.id}
+            title={notice.title}
+            date={notice.date}
+            onClick={handleMoveToNoticeDetail}
+          />
+        ))}
       </S.ContentsBox>
     </SingleButtonLayout>
   );
